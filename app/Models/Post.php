5dda0c21854baa5c92a6268784ecd4c0cd3baa2c
VinceNguyen000 <vluxiphe@gmail.com>
2024-02-03 00:51:03 -0600

    23. Route Model Binding

---
+++ app/Models/Post.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Post extends Model
+{
+    use HasFactory;
+
+    protected $guarded = ['id'];
+
+    public function getRouteKeyName()
+    {
+        return 'slug';
+    }
+}
+
+
+//namespace App\Models;
+//
+//use Illuminate\Database\Eloquent\ModelNotFoundException;
+//use Illuminate\Support\Facades\File;
+//use Spatie\YamlFrontMatter\YamlFrontMatter;
+//
+//class Post
+//{
+//    public $title, $excerpt, $date, $body, $slug;
+//
+//    public function __construct($title, $excerpt, $date, $body, $slug)
+//    {
+//        $this->title = $title;
+//        $this->excerpt = $excerpt;
+//        $this->date = $date;
+//        $this->body = $body;
+//        $this->slug = $slug;
+//    }
+//
+//    //get all files in the path
+//    public static function all()
+//    {
+//        return collect(File::files(resource_path("posts")))
+//            ->map(fn($file) => YamlFrontMatter::parseFile($file))
+//            ->map(fn($docs) => new Post(
+//                $docs->title,
+//                $docs->excerpt,
+//                $docs->date,
+//                $docs->body(),
+//                $docs->slug
+//            ));
+//    }
+//
+//
+//    //find slug in the path to navigate
+//    public static function find($slug)
+//    {
+//        #1
+//        //        if (!file_exists($path = resource_path("posts/{$slug}.html"))) {
+//        //            throw new ModelNotFoundException();
+//        //        }
+//        //
+//        //        return cache()->remember("posts,{$slug}", 1200, fn() => file_get_contents($path));
+//        #2
+//        return static::all()->firstWhere('slug', $slug);
+//    }
+//}
