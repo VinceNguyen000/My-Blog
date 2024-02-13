b5e36da4edcecb13ed0d9b0df41d92656673b996
VinceNguyen000 <vluxiphe@gmail.com>
2024-02-12 23:05:13 -0600

    HTML, CSS to Blade

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -1,21 +1,35 @@
 <x-layout>
+    @include('_post-header')
+    <main class="max-w-6xl mx-auto mt-6 lg:mt-20 space-y-6">
+            <x-post-featured-card />
 
-    @foreach ($posts as $post)
+        <div class="lg:grid lg:grid-cols-2">
+            <x-post-card />
+            <x-post-card />
+        </div>
 
-        <article class="{{$loop->even ? 'foobar' : ''}}">
-            <h1>
-                <a href="/posts/{{ $post->slug}}">
-                    {!! $post->title !!}
-                </a>
-            </h1>
-            <p>
-                By <a href="/authors/{{$post->author->name}}">{{$post->author->name}}</a> in <a href="/categories/{{$post->category->slug}}">{{$post->category->name}}</a> category
-            </p>
-            <div>
-                <p><?= $post->excerpt; ?></p>
-            </div>
+        <div class="lg:grid lg:grid-cols-3">
+            <x-post-card />
+            <x-post-card />
+            <x-post-card />
+        </div>
+    </main>
+{{--    @foreach ($posts as $post)--}}
 
-        </article>
-    @endforeach
+{{--        <article class="{{$loop->even ? 'foobar' : ''}}">--}}
+{{--            <h1>--}}
+{{--                <a href="/posts/{{ $post->slug}}">--}}
+{{--                    {!! $post->title !!}--}}
+{{--                </a>--}}
+{{--            </h1>--}}
+{{--            <p>--}}
+{{--                By <a href="/authors/{{$post->author->name}}">{{$post->author->name}}</a> in <a href="/categories/{{$post->category->slug}}">{{$post->category->name}}</a> category--}}
+{{--            </p>--}}
+{{--            <div>--}}
+{{--                <p><?= $post->excerpt; ?></p>--}}
+{{--            </div>--}}
+
+{{--        </article>--}}
+{{--    @endforeach--}}
 </x-layout>
 
