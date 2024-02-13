7356123f27554fc7abd7fc7faa61605fc6f3cf08
VinceNguyen000 <vluxiphe@gmail.com>
2024-02-13 17:12:35 -0600

    update

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -2,7 +2,7 @@
     @include('_post-header')
     <main class="max-w-6xl mx-auto mt-6 lg:mt-20 space-y-6">
         @if($posts->count())
-            <x-posts-grid :posts="$posts" />
+            <x-posts-grid :posts="$posts"/>
         @else
             <p class="text-center">No Data</p>
         @endif
