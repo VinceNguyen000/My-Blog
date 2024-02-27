78de2bafc5c36c5f608ad5437494c05e2f8ddfc0
VinceNguyen000 <vluxiphe@gmail.com>
2024-02-26 18:00:40 -0600

    Install Framework

--- app/Http/Controllers/SessionController.php
+++ app/Http/Controllers/SessionController.php
@@ -25,17 +25,23 @@ class SessionController extends Controller
             ]
         );
 
-        if(auth()->attempt($logginAttributes)){
+        if(! auth()->attempt($logginAttributes)){
+    //        return back()
+    //            ->withInput()
+    //            ->withErrors(['email'=> 'Cannot find the provided credentials']);
+
+                throw \Illuminate\Validation\ValidationException::withMessages([
+                    'email' => 'Cannot not verify provided credentials!'
+            ]);
+
+            session()->regenerate(); //prevent session fixation attack
+
             return redirect('/')->with('success', 'Welcome Back!');
         }
 
-//        return back()
-//            ->withInput()
-//            ->withErrors(['email'=> 'Cannot find the provided credentials']);
 
-        throw \Illuminate\Validation\ValidationException::withMessages([
-            'email' => 'Cannot not verify provided credentials!'
-        ]);
+
+
 
     }
 }
