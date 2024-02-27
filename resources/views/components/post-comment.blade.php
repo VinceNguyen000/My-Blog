1abd9c2b4ad3b36ca821041bb45724abec21b8d8
VinceNguyen000 <vluxiphe@gmail.com>
2024-02-26 20:32:38 -0600

    Comment section component

---
+++ resources/views/components/post-comment.blade.php
@@ -0,0 +1,20 @@
+<article class="flex bg-gray-100 p-6 rounded-xl border border-gray-200 space-x-6">
+    <div class="flex-shrink-0">
+        <img src="https://i.pravatar.cc/100" alt="" class="width: 100px rounded">
+    </div>
+
+    <div>
+        <header class="mb-4">
+            <h3 class="font-bold">Vince</h3>
+            <p class="text-xs">Posted
+                <time>8 months ago</time>
+            </p>
+        </header>
+        <p>
+            Listen to the song here in my heart
+            A melody I start, but can't complete
+            Listen to the sound from deep within
+            It's only beginning to find release
+        </p>
+    </div>
+</article>
--- resources/views/register/create.blade.php
+++ resources/views/register/create.blade.php
@@ -39,22 +39,6 @@
                 </div>
 
                 <div class="mb-2">
-                    <label for="password"
-                           class="block mb-2 uppercase font-bold text-xs text-gray-700">
-                        Password
-                    </label>
-                    <input type="password"
-                           name="password"
-                           id="password"
-                           class="border border-gray-400 p-2 w-full mb-6"
-                           required
-                    />
-                    @error('password')
-                    <p class="text-red-500 text-xs mt-1">{{ $message }}</p>
-                    @enderror
-                </div>
-
-                <div class="mb-2">
                     <label for="email"
                            class="block mb-2 uppercase font-bold text-xs text-gray-700">
                         Email
@@ -70,6 +54,23 @@
                     <p class="text-red-500 text-xs mt-1">{{ $message }}</p>
                     @enderror
                 </div>
+
+                <div class="mb-2">
+                    <label for="password"
+                           class="block mb-2 uppercase font-bold text-xs text-gray-700">
+                        Password
+                    </label>
+                    <input type="password"
+                           name="password"
+                           id="password"
+                           class="border border-gray-400 p-2 w-full mb-6"
+                           required
+                    />
+                    @error('password')
+                    <p class="text-red-500 text-xs mt-1">{{ $message }}</p>
+                    @enderror
+                </div>
+
                 <div class="mb-2">
                     <button type="submit"
                             class="bg-blue-400 text-white rounded py-2 px-4 hover:bg-blue-500 mb-6"
